b17b6640c32face1ed6b6199a4dc4edcb36dac58
Nyakio-Eva <evalkariuki@gmail.com>
2025-09-07 08:26:25 +0300

    add swagger documentation and celery worker

--- backend_security/ip_tracking/apps.py
+++ backend_security/ip_tracking/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+import os
 
 
 class IpTrackingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend_security.ip_tracking'
+
+class MyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp'
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN", None) != "true":  # prevent double run
+            from celery import current_app
+            current_app.worker_main(["worker", "-A", "backend_security", "-l", "info"])
