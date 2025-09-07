65f549568493afa8335176b3dd95f14523efd6df
Nyakio-Eva <evalkariuki@gmail.com>
2025-09-07 14:30:13 +0300

    debug urls for api

--- backend_security/backend_security/urls.py
+++ backend_security/backend_security/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.generic import RedirectView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,6 +42,9 @@ urlpatterns = [
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    
+    # redirect root ("/") → swagger
+    path("", RedirectView.as_view(url="swagger/", permanent=False)),
 ]
 
 
